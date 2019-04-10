6003f84495d3e11cab8416f15ce67fe00e965f96
yinleon <hello.leonyin@gmail.com>
2019-04-10 09:11:16 -0400

    updated

--- config.py
+++ config.py
@@ -1,17 +1,27 @@
 import os
+import datetime
 
 # change this!
 subreddit = 'dankmemes'
-working_dir = f'/beegfs/work/smapp/reddit_/{subreddit}/'
-
+data_dir = '/beegfs/work/smapp/reddit_'
+working_dir = os.path.join(data_dir, f'{subreddit}/')
+media_dir = os.path.join(data_dir, 'media')
+mosaic_dir = os.path.join(working_dir, 'mosaics')
+output_dir = 'ouput/'
 image_lookup_file = os.path.join(working_dir, 'media.json.gz')
 
 # these files don't exist yet
-logits_file = os.path.join(working_dir, 'image_features_copy.csv.gz')
+logits_file = os.path.join(working_dir, 'image_features.csv.gz')
+full_metadata_file = os.path.join(working_dir, 'full_metadata.csv.gz')
 knn_file = os.path.join(working_dir, 'knn.pkl')
+file_animation = os.path.join(output_dir,'doppler_mosaic.mp4')
 
-# This is where local images are stored
-media_dir = os.path.join(working_dir, 'media')
-
-for _dir in [working_dir, media_dir]:
+for _dir in [working_dir, media_dir, output_dir, mosaic_dir]:
     os.makedirs(_dir, exist_ok=True)
+    
+# shared variables
+skip_hash = ['NOHASH', '0000000000000000', 'nan']
+n_dimensions = 2048 # features from resnet50
+cols_conv_feats = [f'conv_{n}' for n in range(n_dimensions)]
+cutoff_date = datetime.datetime(2019, 1, 1)
+
