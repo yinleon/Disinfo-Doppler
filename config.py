a5cd08facf10beb760012a5c33960d7f0283ab13
chino <hello.leonyin@gmail.com>
2019-04-13 11:10:30 -0400

    some minor changes

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import datetime
 
 # change this!
 subreddit = 'dankmemes'
-data_dir = '/beegfs/work/smapp/reddit_'
+data_dir = '/mnt/n1p1/data/platforms/reddit'
 working_dir = os.path.join(data_dir, f'{subreddit}/')
 media_dir = os.path.join(data_dir, 'media')
 mosaic_dir = os.path.join(working_dir, 'mosaics')
@@ -13,7 +13,8 @@ image_lookup_file = os.path.join(working_dir, 'media.json.gz')
 # these files don't exist yet
 logits_file = os.path.join(working_dir, 'image_features.csv.gz')
 full_metadata_file = os.path.join(working_dir, 'full_metadata.csv.gz')
-knn_file = os.path.join(working_dir, 'knn.pkl')
+sample_dataset_file = os.path.join(working_dir, 'sample_media.csv.gz')
+two_dim_embeddings_file = os.path.join(working_dir, '2d_embeddings.csv')
 file_animation = os.path.join(output_dir,'doppler_mosaic.mp4')
 
 for _dir in [working_dir, media_dir, output_dir, mosaic_dir]:
@@ -21,7 +22,5 @@ for _dir in [working_dir, media_dir, output_dir, mosaic_dir]:
     
 # shared variables
 skip_hash = ['NOHASH', '0000000000000000', 'nan']
-n_dimensions = 2048 # features from resnet50
+n_dimensions = 2048 # features from resnet50, change this is you change the model in feature extraction.
 cols_conv_feats = [f'conv_{n}' for n in range(n_dimensions)]
-cutoff_date = datetime.datetime(2019, 1, 1)
-
